386fca3bac238ce2a80f5b8ef3db7ed16d716dd4
G. Queiroz <gabrieljvnq@gmail.com>
2022-12-19 18:51:34 -0300

    feat: skeleton working

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from '@mui/material'
 
 export default function Document() {
   return (
@@ -6,6 +7,7 @@ export default function Document() {
       <Head />
       <body>
         <Main />
+        <CssBaseline enableColorScheme />
         <NextScript />
       </body>
     </Html>
