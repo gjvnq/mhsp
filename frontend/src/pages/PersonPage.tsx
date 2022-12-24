7fe9de9240de5b5ed568eab256f162e445eec88e
G Queiroz <gabrieljvnq@gmail.com>
2022-12-24 16:49:59 -0300

    feat: working local time display

--- frontend/src/pages/PersonPage.tsx
+++ frontend/src/pages/PersonPage.tsx
@@ -1,10 +1,24 @@
 import { Typography, useTheme } from "@mui/material";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import "./PersonPage.css";
 
 export default function PersonPage() {
   const params = useParams();
   const username = params['person'] || '@darkwiiplayer';
+
+  const [date, setDate] = useState(new Date());
+
+  function refreshClock() {
+    setDate(new Date());
+  };
+
+  useEffect(() => {
+      const timer = setInterval(refreshClock, 1000);
+      return function cleanup() {
+        clearInterval(timer);
+      };
+  }, []);
 
   const theme = useTheme();
 
@@ -20,7 +34,7 @@ export default function PersonPage() {
     </div>
     <div className="grid1">
       <span className="label gray_label">Local time</span>
-      <span className="value">13:57:01 CET</span>
+      <span className="value">{date.toLocaleTimeString('en', {timeZone: 'Europe/Berlin', hourCycle: 'h24', timeZoneName: 'shortOffset', hour: '2-digit', minute: '2-digit', second: undefined})}</span>
       <span className="label gray_label">Fronting</span>
       <span className="value">Unclear, perhaps Marcy or Mir</span>
       <span className="label gray_label">Notes</span>
